842e604a92b1fc62519520ce477b4b6d04cab63b
Ole Kröger <o.kroeger@wikunia.de>
2019-01-06 22:59:28 +0100

    start and end for kopt

--- run_kopt.jl
+++ run_kopt.jl
@@ -18,6 +18,14 @@ function get_args()
             help = "number of neighbors to consider each time"
             default = 100
             arg_type = Int64
+        "--start", "-s"
+            help = "start for i"
+            default = 1
+            arg_type = Int64
+        "--end", "-e"
+            help = "end for i"
+            default = 197769
+            arg_type = Int64
     end
 
     return parse_args(s)
@@ -35,5 +43,5 @@ if isinteractive() == false
         path_fn_out *= ".csv"
     end
 
-    TRP.main_kopt(path_fn_in, path_fn_out, args["k"]; neighbors = args["neighbors"])
+    TRP.main_kopt(path_fn_in, path_fn_out, args["k"]; list_of_i = args["start"]:args["end"], neighbors = args["neighbors"])
 end
